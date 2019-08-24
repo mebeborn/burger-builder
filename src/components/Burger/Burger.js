9069bd8d16ac649726311220a8d9fced39a305ba
mebeborn <mebeborn@gmail.com>
2019-08-24 17:31:12 +0300

    Webpack fix, Burger Component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,26 @@ import React from 'react';
 import BurgerIngridient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
-const burger = () => {
+const burger = (props) => {
+
+  let transforemdIngredients = Object.keys(props.ingredients)
+    .map((ingredient) => {
+      return [...Array(props.ingredients[ingredient])].map((_, index) => {
+        return <BurgerIngridient type={ingredient} key={ingredient + index} />
+      });
+    })
+    .reduce((arr, ell)=>{
+      return arr.concat(ell);
+    }, []);
+
+    if(transforemdIngredients.length === 0)
+      transforemdIngredients = <p>Please start adding ingredients</p>;
+
+  console.log(transforemdIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngridient type={'bread-top'}></BurgerIngridient>
-      <BurgerIngridient type={'cheese'}></BurgerIngridient>
-      <BurgerIngridient type={'bacon'}></BurgerIngridient>
+      {transforemdIngredients}
       <BurgerIngridient type={'bread-bottom'}></BurgerIngridient>
     </div>
   )
--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Aux from '../hoc/ReactAux';
 import Burger from '../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      meat: 0,
+      cheese: 0,
+      salad: 0,
+      bacon: 0
+    }
+  }
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Controller</div>
       </Aux>
     )
